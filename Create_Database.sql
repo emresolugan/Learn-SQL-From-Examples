5161b293eb17792c2bb13529bfc9c79fec5e3fd4
Emre SOLUĞAN <soluganemre94@gmail.com>
2017-11-02 13:30:25 +0300

    first step

---
+++ Create_Database.sql
@@ -0,0 +1,15 @@
+USE master;  
+GO  
+
+--Delete the TestData database if it exists.  
+IF EXISTS(SELECT * from sys.databases WHERE name='TestData')  
+BEGIN  
+    DROP DATABASE TestData;  
+END  
+
+
+CREATE DATABASE TestDB
+-- Database is creating.. --
+
+
+
